264b78227e0db9375845f183812c5fb6f578c2af
Joshua Peck <jpeck@amctechnology.com>
2024-06-13 14:56:42 -0400

    IC

---
+++ src/app/customapp.component.ts
@@ -0,0 +1,46 @@
+import { IContextualContact } from './../../node_modules/@amc-technology/ui-library/node_modules/@amc-technology/davinci-api/dist/models/ContextualOperation.d';
+import { CHANNEL_TYPES, IAppConfiguration, ISupportedChannel, initializeComplete, registerContextualControls, registerOnPresenceChanged, setAppHeight, setPresence, setSupportedChannels } from '@amc-technology/davinci-api';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'CustomDaVinciApp';
+
+  appConfig?: IAppConfiguration;
+
+  ngOnInit(): void {
+      this.init().then(() => console.log("Done loading app!"));
+  }
+
+  async init(): Promise<void> {
+    try {
+      registerOnPresenceChanged(async (presence, reason, app) => {
+        if (app != 'DAVINCI APP FOR CALL SIMULATOR')
+        setPresence(presence, reason, '', '');
+      });
+
+      let channel: ISupportedChannel = {
+        channelType: CHANNEL_TYPES.Telephony,
+        idName: "CTI_CHANNEL",
+        id: "CTI1"
+      };
+
+      setSupportedChannels([
+        channel
+      ]);
+
+      registerContextualControls(async (contact, channelType) => {
+      });
+
+      setPresence('Ready', '', '', '');
+      setAppHeight(50);
+      initializeComplete().then((config) => this.appConfig = config);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
